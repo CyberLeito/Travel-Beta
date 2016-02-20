eb4cfdb2723f42101637592ced4c8e50f242a2b6
CyberLeito <munawwar_mi@hotmail.com>
2016-02-20 21:41:54 +0800

    Initial commit, Added BirthDate(Register.aspx), changed TableName(IdentityModels.cs)

---
+++ Travel Beta/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Travel_Beta.Startup))]
+namespace Travel_Beta
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
