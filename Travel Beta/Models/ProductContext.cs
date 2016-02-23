643bc3c0a83fe51b99c11622c20e324f1d7e7b43
CyberLeito <munawwar_mi@hotmail.com>
2016-02-23 22:17:36 +0800

    Incomplete Procuct/catagory added Admin almost incomplete. Homepage and application name not changed

---
+++ Travel Beta/Models/ProductContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace Travel_Beta.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("DefaultConnection")
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    
+    }
+}
